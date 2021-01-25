1acae8bffe7d0611920952a482daa6432d82eee9
Oldahir Gomez <oldahir_gomez25@hotmail.com>
2021-01-24 22:50:36 -0600

    Aplicacion de Cartelera de Peliculas

---
+++ src/app/services/peliculas.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { CarteleraResponse, Movie } from '../interfaces/cartelera-response';
+import  { map, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PeliculasService {
+  private baseURL: string = 'https://api.themoviedb.org/3';
+  private carteleraPage = 1;
+  public cargando: boolean = false;
+
+  constructor( private http: HttpClient ) { }
+
+  get params() {
+    return {
+      api_key: 'd64016245a078ca218d1cadeb2fadba1',
+      language: 'es-ES',
+      page: this.carteleraPage.toString()
+    }
+  }
+
+  getCartelera(): Observable<Movie[]> {
+    if (this.cargando) {
+      // cargando pelicula
+      return of([]);
+    }
+
+    this.cargando = true;
+    return this.http.get<CarteleraResponse>(`${this.baseURL}/movie/now_playing`, {
+      params: this.params
+    }).pipe(
+      map( (resp) => resp.results ),
+      tap( () => {
+        this.carteleraPage += 1;
+        this.cargando = false;
+      })
+    );
+  }
+}
